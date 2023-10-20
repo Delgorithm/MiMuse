6d9d3b6394aad003b469559be65a0c212f504804
Delgorithm <delgorithm@gmail.com>
2023-10-20 14:38:45 +0200

    fix : User is now able to connect and see its informations

--- src/components/SpotifyAuth.jsx
+++ src/components/SpotifyAuth.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-
 async function fetchTrackData(accessToken) {
     const trackId = '2TpxZ7JUBn3uw46aR7qd6V';
     const response = await fetch(`https://api.spotify.com/v1/tracks/${trackId}`, {
@@ -18,27 +17,26 @@ async function fetchTrackData(accessToken) {
     return data;
 }
 
+async function generateCodeVerifier(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
 const SpotifyAuth = () => {
     const [profile, setProfile] = useState(null);
 
-
-    async function generateCodeChallenge(codeVerifier) {
-        const data = new TextEncoder().encode(codeVerifier);
-        const digest = await window.crypto.subtle.digest('SHA-256', data);
-        return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-            .replace(/\+/g, '-')
-            .replace(/\//g, '_')
-            .replace(/=+$/, '');
-    }
-
     async function getAccessToken(clientId, code) {
         const verifier = localStorage.getItem("verifier");
-    
         const params = new URLSearchParams();
         params.append("client_id", clientId);
         params.append("grant_type", "authorization_code");
         params.append("code", code);
-        params.append("redirect_uri", "http://localhost:5173");
+        params.append("redirect_uri", "http://localhost:5173/");
         params.append("code_verifier", verifier);
     
         try {
@@ -50,17 +48,20 @@ const SpotifyAuth = () => {
                 body: params
             });
     
+            const data = await response.json();
+    
             if (!response.ok) {
-                throw new Error(`Failed to get access token: ${response.status} - ${response.statusText}`);
+                console.error("Error from Spotify API:", data);
+                throw new Error(`Failed to get access token: ${response.status} - ${data.error.message}`);
             }
     
-            const data = await response.json();
             return data.access_token;
         } catch (error) {
-            console.error(error);
+            console.error("Error during access token request:", error);
             throw error;
         }
     }
+    
 
     async function fetchProfile(token) {
         try {
@@ -86,9 +87,9 @@ const SpotifyAuth = () => {
             const params = new URLSearchParams(window.location.search);
             const code = params.get('code');
             const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-            
+
             if (!code) {
-                const verifier = generateCodeVerifier(128);
+                const verifier = await generateCodeVerifier(128);
                 redirectToAuthCodeFlow(clientId, verifier);
             } else {
                 try {
@@ -111,12 +112,24 @@ const SpotifyAuth = () => {
         const params = new URLSearchParams();
         params.append("client_id", clientId);
         params.append("response_type", "code");
-        params.append("redirect_uri", "http://localhost:5173/callback");
+        params.append("redirect_uri", "http://localhost:5173/");
         params.append("scope", "user-read-private user-read-email");
         params.append("code_challenge_method", "S256");
         params.append("code_challenge", challenge);
 
-        document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+        window.location.href = `https://accounts.spotify.com/authorize?${params.toString()}`;
+    }
+
+    async function generateCodeChallenge(codeVerifier) {
+        const encoder = new TextEncoder();
+        const data = encoder.encode(codeVerifier);
+        const hashedBuffer = await crypto.subtle.digest('SHA-256', data);
+        const base64String = btoa(String.fromCharCode(...new Uint8Array(hashedBuffer)))
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_')
+            .replace(/=+$/, '');
+
+        return base64String;
     }
 
     return (
