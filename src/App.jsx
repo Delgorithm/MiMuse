ae0e9cdd26438db24b1139b8c18d8bd3efd44a02
Delgorithm <delgorithm@gmail.com>
2023-10-25 10:22:48 +0200

    fix : redirections for each categories done

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Pages/Home/Home';
 import Category from './components/Pages/Category/Category';
-import Topalbums from './components/Pages/Category/TopAlbums/Topalbums';
 import Topartists from './components/Pages/Category/TopArtists/Topartists';
+import Topalbums from './components/Pages/Category/TopAlbums/Topalbums';
 import Toptracks from './components/Pages/Category/TopTracks/Toptracks';
 import Toptracksmonthly from './components/Pages/Category/TopTracksMonthly/Toptracksmonthly';
 import Toptracksweekly from './components/Pages/Category/TopTracksWeekly/Toptracksweekly';
@@ -14,13 +14,14 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/category' element={<Category />}>
-          <Route path='Topalbums' element={<Topalbums />} />
-          <Route path='Topartists' element={<Topartists />} />
-          <Route path='Toptracks' element={<Toptracks />} />
-          <Route path='Toptracksweekly' element={<Toptracksweekly />} />
-          <Route path='Toptracksmonthly' element={<Toptracksmonthly />} />
-          <Route path='Toptracksyearly' element={<Toptracksyearly />} />
+        <Route path='/category'>
+          <Route index element={<Category />} />
+          <Route path=':toptracks' element={<Toptracks />} />
+          <Route path=':topartists' element={<Topartists />} />
+          <Route path=':topalbums' element={<Topalbums />} />
+          <Route path=':toptracksweekly' element={<Toptracksweekly />} />
+          <Route path=':toptracksmonthly' element={<Toptracksmonthly />} />
+          <Route path=':toptracksyearly' element={<Toptracksyearly />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Pages/Category/Category.jsx
+++ src/components/Pages/Category/Category.jsx
@@ -1,64 +1,77 @@
 import React from 'react'
 import Nav from '../../Nav/Nav'
 import categoryData from '../../data/categoryData'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Category = () => {
 
     const { topArtists, topTracks, topAlbums, topTracksWeekly, topTracksMonthly, topTracksYearly } = categoryData;
+    const navigate = useNavigate();
 
   return (
     <div className='bg-blackSpotify h-full text-white'>
         <Nav />
         <div className='p-6 flex flex-col gap-6 h-screen'>
 
-            <Link to='/toptracks'>
                 {topTracks.map((item, index) => (
-                    <button key={item.id} className='p-5 w-full bg-red-400 rounded-xl transformation duration-200 ease-in-out hover:scale-105 active:opacity-40'>
+                    <button 
+                        key={item.id} 
+                        className='p-5 w-full bg-red-400 rounded-xl transformation duration-200 ease-in-out hover:scale-105 active:opacity-40'
+                        onClick={() => navigate('/category/toptracks')}    
+                    >
                         <p>{item.title}</p>
                     </button>
                 ))}
-            </Link>
 
-            <Link to='/topartists'>
                 {topArtists.map((item, index) => (
-                    <button key={item.id} className='p-5 w-full  bg-purple-400 rounded-xl transformation duration-200 ease-in-out hover:scale-105 active:opacity-40'>
+                    <button 
+                        key={item.id} 
+                        className='p-5 w-full  bg-purple-400 rounded-xl transformation duration-200 ease-in-out hover:scale-105 active:opacity-40'
+                        onClick={() => navigate('/category/topartists')}      
+                    >
                         <p>{item.title}</p>
                     </button>
                 ))}
-            </Link>
 
-            <Link to='/topalbums'>
                 {topAlbums.map((item, index) => (
-                    <button key={item.id} className='p-5 w-full  bg-blue-300 rounded-xl transformation duration-200 ease-in-out hover:scale-105 active:opacity-40'>
+                    <button 
+                        key={item.id} 
+                        className='p-5 w-full  bg-blue-300 rounded-xl transformation duration-200 ease-in-out hover:scale-105 active:opacity-40'
+                        onClick={() => navigate('/category/topalbums')}      
+                    >
                         <p>{item.title}</p>
                     </button>
                 ))}
-            </Link>
 
-            <Link to='/toptracksweekly'>
                 {topTracksWeekly.map((item, index) => (
-                    <button key={item.id} className='p-5 w-full  bg-yellow-400 rounded-xl transformation duration-200 ease-in-out hover:scale-105 active:opacity-40'>
+                    <button 
+                        key={item.id} 
+                        className='p-5 w-full  bg-yellow-400 rounded-xl transformation duration-200 ease-in-out hover:scale-105 active:opacity-40'
+                        onClick={() => navigate('/category/toptracksweekly')}  
+                    >
                         <p>{item.title}</p>
                     </button>
                 ))}
-            </Link>
 
-            <Link to='/toptracksmonthly'>
                 {topTracksMonthly.map((item, index) => (
-                    <button key={item.id} className='p-5 w-full  bg-gray-400 rounded-xl transformation duration-200 ease-in-out hover:scale-105 active:opacity-40'>
+                    <button 
+                        key={item.id} 
+                        className='p-5 w-full  bg-gray-400 rounded-xl transformation duration-200 ease-in-out hover:scale-105 active:opacity-40'
+                        onClick={() => navigate('/category/toptracksmonthly')}  
+                    >
                         <p>{item.title}</p>
                     </button>
                 ))}
-            </Link>
 
-            <Link to='/toptracksyearly'>
                 {topTracksYearly.map((item, index) => (
-                    <button key={item.id} className='p-5 w-full  bg-orange-400 rounded-xl transformation duration-200 ease-in-out hover:scale-105 active:opacity-40'>
+                    <button 
+                        key={item.id} 
+                        className='p-5 w-full  bg-orange-400 rounded-xl transformation duration-200 ease-in-out hover:scale-105 active:opacity-40'
+                        onClick={() => navigate('/category/toptracksyearly')}      
+                    >
                         <p>{item.title}</p>
                     </button>
                 ))}
-            </Link>
         </div>
     </div>
   )
